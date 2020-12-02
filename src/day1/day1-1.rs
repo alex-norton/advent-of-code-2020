f2ba68fa34c7324d75294f1dce19cf35f794e4ee
Alex <alexander.r.norton@gmail.com>
2020-12-01 22:16:42 -0800

    day 1

---
+++ src/day1/day1-1.rs
@@ -0,0 +1,34 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::collections::HashSet;
+
+fn main() {
+    let mut nums: Vec<i32> = Vec::new();
+    let mut targets = HashSet::new();
+    
+    if let Ok(lines) = read_lines("./input") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                let num = ip.parse().unwrap();
+                nums.push(num);
+                targets.insert(2020 - num);
+            }
+        }
+    }
+    for num in nums {
+        if targets.contains(&num) {
+            println!("{}, {}", num, 2020 - num)
+            // found 1014, 1006 for output 1020084
+        }
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
---
+++ src/day1/day1-2.rs
@@ -0,0 +1,35 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let mut nums: Vec<i32> = Vec::new();
+    
+    if let Ok(lines) = read_lines("./input") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines {
+            if let Ok(ip) = line {
+                let num = ip.parse().unwrap();
+                nums.push(num);
+            }
+        }
+    }
+    for num1 in &nums {
+        for num2 in &nums {
+            for num3 in &nums{
+                if num1 + num2 + num3 == 2020 {
+                    println!("{}, {}, {}", num1, num2, num3)
+                    // found 780, 542, 698
+                }
+            }
+        }
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
