b8145c81a09df8218b208a17599d9570919859b8
Alex <alexander.r.norton@gmail.com>
2020-12-05 01:07:20 -0800

    More question marks whoo

--- src/bin/day5-2.rs
+++ src/bin/day5-2.rs
@@ -1,20 +1,21 @@
 use std::cmp;
 use std::fs::read_to_string;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut max = isize::MIN;
     let mut min = isize::MAX;
     let mut total: isize = 0;
-    for line in read_to_string("data/day5input").unwrap().split('\n') {
+    for line in read_to_string("data/day5input")?.split('\n') {
         let line_str: String = line
             .chars()
             .map(|x| if x == 'F' || x == 'L' { '0' } else { '1' })
             .collect();
-        let line_num = isize::from_str_radix(&line_str, 2).unwrap();
+        let line_num = isize::from_str_radix(&line_str, 2)?;
         total += line_num;
         max = cmp::max(max, line_num);
         min = cmp::min(min, line_num);
     }
     let expected = (min + max) * (max - min + 1) / 2;
     println!("{}", expected - total);
+    Ok(())
 }
