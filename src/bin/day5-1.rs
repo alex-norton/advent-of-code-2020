ce90f9c4c3d725cc40a7fdfe1e1804bdbcbf6583
Alex <alexander.r.norton@gmail.com>
2020-12-05 01:01:48 -0800

    Day 5

---
+++ src/bin/day5-1.rs
@@ -0,0 +1,17 @@
+use std::fs::read_to_string;
+
+fn main() {
+    let max = read_to_string("data/day5input")
+        .unwrap()
+        .split('\n')
+        .map(|line| {
+            let line_str: String = line
+                .chars()
+                .map(|x| if x == 'F' || x == 'L' { '0' } else { '1' })
+                .collect();
+            usize::from_str_radix(&line_str, 2).unwrap()
+        })
+        .max()
+        .unwrap();
+    println!("{}", max);
+}
---
+++ src/bin/day5-2.rs
@@ -0,0 +1,20 @@
+use std::cmp;
+use std::fs::read_to_string;
+
+fn main() {
+    let mut max = isize::MIN;
+    let mut min = isize::MAX;
+    let mut total: isize = 0;
+    for line in read_to_string("data/day5input").unwrap().split('\n') {
+        let line_str: String = line
+            .chars()
+            .map(|x| if x == 'F' || x == 'L' { '0' } else { '1' })
+            .collect();
+        let line_num = isize::from_str_radix(&line_str, 2).unwrap();
+        total += line_num;
+        max = cmp::max(max, line_num);
+        min = cmp::min(min, line_num);
+    }
+    let expected = (min + max) * (max - min + 1) / 2;
+    println!("{}", expected - total);
+}
