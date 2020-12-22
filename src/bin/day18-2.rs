4f27f275837f0ee8e677de741dc730c299b3d76b
Alex <alexander.r.norton@gmail.com>
2020-12-21 19:56:38 -0800

    Day 18-2

--- src/bin/day18-2.rs
+++ src/bin/day18-2.rs
@@ -1,58 +1,54 @@
 use std::fs::read_to_string;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let sum = read_to_string("data/test")?
+    let sum = read_to_string("data/day18input")?
         .lines()
-        .map(|x| calculate(0, &mut x.chars(), false))
+        .map(|x| calculate(&mut x.chars(), false))
         .sum::<u64>();
     println!("{}", sum);
     Ok(())
 }
 
-fn calculate(num: u64, cs: &mut std::str::Chars, consume_paren: bool) -> u64 {
-    let mut total: u64 = num;
-    let mut op: Option<char> = None;
+/*
+    This is a messy hack to extend the method of Part 1.
+    The intuition is that pluses should evalulate greedily,
+    whereas multiplications should evaluate lazily.
+    We handle the latter through recursion, but parentheses are
+    already handled through recursion. In part 1, every step consumed
+    a character, but that made multiplication recursion (which should return
+    without consuming the right paren) and parenthesis recursion (which
+    should consume the right paren) indistinguishable. To hack around that,
+    there's this "consume_paren" boolean which uses chars::as_str to do the
+    equivalent of peeking, to allow multiplication recursions to peek the ')'
+    and return without consuming it.
+*/
+fn calculate(cs: &mut std::str::Chars, consume_paren: bool) -> u64 {
+    let mut total: u64 = 0;
+    let mut is_plus = false;
     loop {
         if !consume_paren {
             let x = cs.as_str();
             if let Some(c) = x.chars().next() {
                 if c == ')' {
-                    println!("Peeked ), returning {}", total);
                     return total;
                 }
             }
         }
         if let Some(c) = cs.next() {
             match c {
-                ')' => {
-                    println!("Consumed ), returning {}", total);
-                    return total;
-                }
+                ')' => return total,
                 ' ' => continue,
-                '+' => op = Some('+'),
-                '*' => {
-                    total *= {
-                        println!("Multiply, recursing");
-                        let val = calculate(num, cs, false);
-                        println!("Multiplying {} by {}", total, val);
-                        val
-                    }
-                }
+                '+' => is_plus = true,
+                '*' => total *= calculate(cs, false),
                 c => {
                     let num = match c {
-                        '(' => {
-                            println!("Consumed (, recursing");
-                            calculate(0, cs, true)
-                        }
+                        '(' => calculate(cs, true),
                         c => c.to_digit(10).unwrap() as u64,
                     };
-                    match op {
-                        None => total = num,
-                        Some('+') => {
-                            println!("Adding {} to {}", num, total);
-                            total += num
-                        }
-                        _ => panic!(),
+                    if is_plus {
+                        total += num;
+                    } else {
+                        total = num;
                     }
                 }
             }
@@ -60,6 +56,5 @@ fn calculate(num: u64, cs: &mut std::str::Chars, consume_paren: bool) -> u64 {
             break;
         }
     }
-    println!("End of string, returning {}", total);
     total
 }
