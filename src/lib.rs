27ac8749787bb5a07768a8d35b8869302b6d3b8c
Alex <alexander.r.norton@gmail.com>
2020-12-30 15:12:29 -0800

    Lib note

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 
+// Belated note: I thought I'd be accumulating useful snippets as I went.
+// That was not the case! Even this was unnecessary once I found the right
+// standard lib function.
+
 pub fn read_lines(filename: &str) -> io::Lines<io::BufReader<File>> {
   let file = File::open(filename).unwrap();
   io::BufReader::new(file).lines()
